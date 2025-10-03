d9e937d961183c24a328bd02793a132a6ba076d2
pasindu-piyumal <22cds0423@ms.sab.ac.lk>
2025-10-04 01:05:35 +0530

    create sorage for storing and retrieving data

--- storage.py
+++ storage.py
@@ -1,1 +1,14 @@
-print("T")
+import json
+import os
+
+taskFile = "Task.json"
+
+def load_task():
+    if os.paht.exists(taskFile):
+        with open(taskFile, "r") as file:
+            return json.load(taskFile)
+    return []
+
+def save_tasks(tasks):
+    with open(taskFile, "w") as file:
+        json.dump(tasks, file, indent=4)
